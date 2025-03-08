338ebdfa3eaee4afd11e916af0852abcb1a03f15
leavesster <11785335+leavesster@users.noreply.github.com>
2025-03-08 13:18:13 +0800

    feat(action): support build local

--- npm/npm.mjs
+++ npm/npm.mjs
@@ -2,12 +2,42 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import { packageTemplate, readme, targetConfig } from "./target.mjs";
 import { getVersion, __dirname } from "./utils.mjs";
+import { spawn } from "node:child_process";
 
 const targets = Object.keys(targetConfig);
 const version = await getVersion();
 
 const owner = process.env.GITHUB_REPOSITORY_OWNER || "oomol";
 const projectDir = path.dirname(__dirname);
+
+// build the @oomol/oocana-cli package.json with binaries
+if (!process.env.CI) {
+    console.log("build outside of CI");
+    for (const target of targets) {
+        const cargo = spawn("cargo", ["build", "--release", "--target",
+            target], {
+            cwd: projectDir,
+            env: {
+                ...process.env,
+                // you need install gcc for linux targets
+                CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER: "x86_64-linux-gnu-gcc",
+                CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER: "aarch64-linux-gnu-gcc",
+            }
+        });
+        cargo.stdout.pipe(process.stdout);
+        cargo.stderr.pipe(process.stderr);
+        await new Promise((resolve, reject) => {
+            cargo.on("close", (code) => {
+                if (code === 0) {
+                    resolve();
+                } else {
+                    reject(new Error(`Cargo build failed for target ${target} with code ${code}`));
+                }
+            });
+        });
+        console.log(`Cargo build succeeded for target ${target}`);
+    }
+}
 
 // @oomol/oocana-cli-* package.json with binaries
 for (const target of targets) {
@@ -35,5 +65,7 @@ for (const target of targets) {
     await fs.writeFile(path.join(targetDir, "package.json"), JSON.stringify(newFile, null, 2));
     await fs.writeFile(path.join(targetDir, "README.md"), readme(target).replace(/@oomol/g, `@${owner}`));
     await fs.copyFile(path.join(__dirname, "index.js"), path.join(targetDir, "index.js"));
+    console.log(`Built @${owner}/oocana-cli-${target} package.json`);
+
 }
 
