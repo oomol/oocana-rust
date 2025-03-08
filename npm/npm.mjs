cdbe49e6f3004e8f393d0ca730f48a588d77989f
leavesster <11785335+leavesster@users.noreply.github.com>
2025-03-08 15:09:18 +0800

    chore(action): fix release has no executable permission (#2)
    * fix: use tar.gz to avoid missing executable permission
    
    * refactor: avoid download binary from release and delete oocana-cli package
    
    * feat(action): support build local
    
    * feat: support publish for everyone
    
    * avoid artifact conflict
    
    * fix working directory
    
    * add log
    
    * fix download directory
    
    * add filter for trigger
    
    * chore: v0.23.1

--- npm/npm.mjs
+++ npm/npm.mjs
@@ -1,29 +1,42 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 import { packageTemplate, readme, targetConfig } from "./target.mjs";
-import { downloadFile, getVersion, __dirname } from "./utils.mjs";
+import { getVersion, __dirname } from "./utils.mjs";
+import { spawn } from "node:child_process";
 
 const targets = Object.keys(targetConfig);
 const version = await getVersion();
 
 const owner = process.env.GITHUB_REPOSITORY_OWNER || "oomol";
+const projectDir = path.dirname(__dirname);
 
-// @oocana/oocana-cli package.json without binaries
-const oocana_cli_package = path.join(__dirname, "oocana-cli");
-const oocana_cli_bin = path.join(oocana_cli_package, "bin");
-{
-    await fs.mkdir(oocana_cli_bin, { recursive: true });
-
-    const newFile = {
-        name: `@${owner}/oocana-cli`,
-        description: `All-platform cli binaries for @${owner}/oocana`,
-        version,
-        ...packageTemplate
-    };
-
-    await fs.writeFile(path.join(oocana_cli_package, "package.json"), JSON.stringify(newFile, null, 2));
-    await fs.writeFile(path.join(oocana_cli_package, "README.md"), `# @${owner}/oocana-cli\n\n${newFile.description}`);
-    await fs.copyFile(path.join(__dirname, "index.js"), path.join(oocana_cli_package, "index.js"));
+// build the @oomol/oocana-cli package.json with binaries
+if (!process.env.CI) {
+    console.log("build outside of CI. be aware the owner is @oomol not your github username!");
+    for (const target of targets) {
+        const cargo = spawn("cargo", ["build", "--release", "--target",
+            target], {
+            cwd: projectDir,
+            env: {
+                ...process.env,
+                // you need install gcc for linux targets
+                CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER: "x86_64-linux-gnu-gcc",
+                CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER: "aarch64-linux-gnu-gcc",
+            }
+        });
+        cargo.stdout.pipe(process.stdout);
+        cargo.stderr.pipe(process.stderr);
+        await new Promise((resolve, reject) => {
+            cargo.on("close", (code) => {
+                if (code === 0) {
+                    resolve();
+                } else {
+                    reject(new Error(`Cargo build failed for target ${target} with code ${code}`));
+                }
+            });
+        });
+        console.log(`Cargo build succeeded for target ${target}`);
+    }
 }
 
 // @oomol/oocana-cli-* package.json with binaries
@@ -32,20 +45,17 @@ for (const target of targets) {
     const binDir = path.join(targetDir, "bin");
     await fs.mkdir(binDir, { recursive: true });
 
-    const oocana = (path.join(binDir, "oocana"))
+    const bin = path.join(binDir, "oocana");
+    const oocana = path.join(projectDir, "target", target, "release", "oocana");
 
-    // TODO: consider build locally if the 404.
-    await downloadFile(`https://github.com/${owner}/oocana-rust/releases/download/v${version}/oocana-cli-${target}`, oocana);
-
-    // copy the oocana binary to npm/oocana-cli/bin
-    await fs.copyFile(oocana, path.join(oocana_cli_bin, target));
+    await fs.copyFile(oocana, bin);
 
     const newFile = {
         name: `@${owner}/oocana-cli-${target}`,
         description: `The ${target} cli binary for @${owner}/oocana`,
         version,
         bin: {
-            "oocana": `bin/${target}`
+            "oocana": `bin/oocana`,
         },
         os: targetConfig[target].os,
         cpu: targetConfig[target].cpu,
@@ -55,5 +65,7 @@ for (const target of targets) {
     await fs.writeFile(path.join(targetDir, "package.json"), JSON.stringify(newFile, null, 2));
     await fs.writeFile(path.join(targetDir, "README.md"), readme(target).replace(/@oomol/g, `@${owner}`));
     await fs.copyFile(path.join(__dirname, "index.js"), path.join(targetDir, "index.js"));
+    console.log(`Built @${owner}/oocana-cli-${target} package.json`);
+
 }
 
