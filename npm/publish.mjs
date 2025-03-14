21b297e1f7e1a3b6b784ddb9a72e602487e02c6d
leavesster <11785335+leavesster@users.noreply.github.com>
2025-03-14 16:52:21 +0800

    chore(action): publish npm registry as well

--- npm/publish.mjs
+++ npm/publish.mjs
@@ -2,9 +2,25 @@ import fs from "node:fs";
 import path from "node:path";
 import { spawnSync } from "node:child_process";
 import { __dirname } from "./utils.mjs";
+import { argv } from "node:process";
 
+
+const registry = argv[2];
+
+console.log("Publishing packages... to", registry || "github");
 for (const pkg of fs.readdirSync(path.join(__dirname))) {
     if (pkg.startsWith("oocana-cli")) {
+
+        if (registry == "npm") {
+            const { status } = spawnSync("npm", ["pkg", "set", "publishConfig.registry=https://registry.npmjs.org"], {
+                cwd: path.join(__dirname, pkg),
+                stdio: "inherit",
+            });
+            if (status !== 0) {
+                process.exit(status);
+            }
+        }
+
         const { status } = spawnSync("npm", ["publish"], {
             cwd: path.join(__dirname, pkg),
             stdio: "inherit",
