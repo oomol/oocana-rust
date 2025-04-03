0b5e27fbe006d3c7e63c050dde6aee51cbbc5d58
leavesster <11785335+leavesster@users.noreply.github.com>
2025-04-03 16:49:13 +0800

    refactor: read from config file

--- utils/src/settings.rs
+++ utils/src/settings.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
 
 use dirs::home_dir;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 
 use serde::{Deserialize, Serialize};
 
@@ -32,19 +34,51 @@ pub struct Config {
     pub run: Option<RunConfig>,
 }
 
-// 全局共享的内容，用于存储一些可以共用的信息。比如 package layer 的信息
-pub fn global_dir() -> Option<PathBuf> {
-    home_dir().map(|mut h| {
-        h.push(".oomol-studio");
-        h.push("oocana");
-        h
-    })
+lazy_static! {
+    pub static ref GLOBAL_CONFIG: Mutex<Config> = Mutex::new(Config {
+        global: None,
+        run: None,
+    });
 }
 
-// 单次信息，无法跨越多个 flow 共享（目前是容器内共用），比如 flow 的运行日志，flow 的 injection layer 等。
+pub fn update_config(config: Config) {
+    let mut global_config = GLOBAL_CONFIG.lock().unwrap();
+    *global_config = config;
+}
+
+pub fn global_dir() -> Option<PathBuf> {
+    let global_config = GLOBAL_CONFIG.lock().unwrap();
+
+    let store_dir = global_config
+        .global
+        .as_ref()
+        .and_then(|g| g.store_dir.as_ref())
+        .map(|s| PathBuf::from(s));
+
+    match store_dir {
+        Some(store_dir) => Some(store_dir),
+        None => home_dir().map(|mut h| {
+            h.push(".oomol-studio");
+            h.push("oocana");
+            h
+        }),
+    }
+}
+
 pub fn oocana_dir() -> Option<PathBuf> {
-    home_dir().map(|mut h| {
-        h.push(".oocana");
-        h
-    })
+    let global_config = GLOBAL_CONFIG.lock().unwrap();
+
+    let oocana_dir = global_config
+        .global
+        .as_ref()
+        .and_then(|g| g.oocana_dir.as_ref())
+        .map(|s| PathBuf::from(s));
+
+    match oocana_dir {
+        Some(oocana_dir) => Some(oocana_dir),
+        None => home_dir().map(|mut h| {
+            h.push(".oocana");
+            h
+        }),
+    }
 }
