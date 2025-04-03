a17689bbe33d44a609d462cc9b7077fff68799a8
leavesster <11785335+leavesster@users.noreply.github.com>
2025-04-03 16:34:27 +0800

    feat: support read from config file

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use human_panic::setup_panic;
 #[cfg(debug_assertions)]
 extern crate better_panic;
 
+use dirs::home_dir;
 #[cfg(feature = "backtrace")]
 use std::env;
 
-use utils::error::Result;
+use config::{Config, File};
+use utils::{error::Result, settings::Config as GlobalConfig, settings::update_config};
 
 /// The main entry point of the application.
 fn main() -> Result<()> {
@@ -31,6 +33,30 @@ fn main() -> Result<()> {
             .verbosity(better_panic::Verbosity::Full)
             .install();
     }
+
+    let config = if let Some(home_dir) = home_dir() {
+        let config_path = home_dir
+            .join(".oocana")
+            .join("config")
+            .to_string_lossy()
+            .to_string();
+        Config::builder()
+            .add_source(File::with_name(&config_path))
+            .build()
+            .ok()
+    } else {
+        None
+    };
+
+    config.map(|c| {
+        c.try_deserialize::<GlobalConfig>()
+            .map(|config| {
+                update_config(config);
+            })
+            .unwrap_or_else(|e| {
+                eprintln!("Failed to load config: {}", e);
+            });
+    });
 
     // Match Commands
     cli::cli_match()?;
