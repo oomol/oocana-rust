79983ed084485335b6abb5b433071390565141fe
leavesster <11785335+leavesster@users.noreply.github.com>
2025-04-03 21:24:40 +0800

    chore: add more test case

--- utils/tests/test.rs
+++ utils/tests/test.rs
@@ -12,18 +12,33 @@ mod tests {
 
         let config = config.unwrap();
         let home_dir = dirs::home_dir().unwrap().to_string_lossy().to_string();
-
+        let global = config.global.clone();
         assert_eq!(
-            config.global.clone().map(|g| g.store_dir.clone()).flatten(),
+            global.clone().map(|g| g.store_dir.clone()).flatten(),
             Some(format!("{}/.oomol-studio/oocana", home_dir))
         );
         assert_eq!(
-            config
-                .global
-                .clone()
-                .map(|g| g.oocana_dir.clone())
-                .flatten(),
+            global.clone().map(|g| g.oocana_dir.clone()).flatten(),
             Some(format!("{}/.oocana", home_dir))
         );
+        assert_eq!(
+            global.clone().map(|g| g.env_file.clone()).flatten(),
+            Some(format!("{}/.env", home_dir))
+        );
+        assert_eq!(
+            global.clone().map(|g| g.bind_path_file.clone()).flatten(),
+            Some(format!("{}/bind_path.env", home_dir))
+        );
+
+        let run = config.run.clone().unwrap();
+        assert_eq!(run.search_paths.is_some(), true);
+        assert_eq!(run.search_paths.unwrap().len(), 0);
+
+        assert_eq!(run.exclude_packages.is_some(), true);
+        assert_eq!(run.exclude_packages.unwrap().len(), 0);
+
+        let extra = run.extra.clone().unwrap();
+        assert_eq!(extra.search_paths.is_some(), true);
+        assert_eq!(extra.search_paths.unwrap().len(), 0);
     }
 }
