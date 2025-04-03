b10772bbca50f33c399debaf2f55bf2e9097753b
leavesster <11785335+leavesster@users.noreply.github.com>
2025-04-03 15:26:09 +0800

    feat: add setting struct

--- utils/src/settings.rs
+++ utils/src/settings.rs
@@ -2,6 +2,36 @@ use std::path::PathBuf;
 
 use dirs::home_dir;
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GlobalConfig {
+    pub store_dir: Option<String>,
+    pub oocana_dir: Option<String>,
+    pub env_file: Option<String>,
+    pub bind_path_file: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RunExtraConfig {
+    pub search_path: Option<Vec<String>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RunConfig {
+    pub search_path: Option<Vec<String>>,
+    pub exclude_packages: Option<Vec<String>>,
+    pub reporter: Option<bool>,
+    pub debug: Option<bool>,
+    pub extra: Option<RunExtraConfig>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub global: Option<GlobalConfig>,
+    pub run: Option<RunConfig>,
+}
+
 // 全局共享的内容，用于存储一些可以共用的信息。比如 package layer 的信息
 pub fn global_dir() -> Option<PathBuf> {
     home_dir().map(|mut h| {
