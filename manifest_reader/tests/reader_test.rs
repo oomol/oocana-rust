50718174cb420c5ff1ec51bfdffa9b6dc7f268ab
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-01 18:06:35 +0800

    refactor: move to test mod and refactor

--- manifest_reader/tests/reader_test.rs
+++ manifest_reader/tests/reader_test.rs
@@ -1,20 +1,19 @@
-use manifest_reader::{path_finder::find_flow, reader::read_flow_block};
-use std::path::PathBuf;
-use utils::error::Result;
+#[cfg(test)]
+mod tests {
+    use manifest_reader::reader::{read_flow_block, read_task_block};
+    use std::path::PathBuf;
+    use utils::error::Result;
+    fn test_directory() -> PathBuf {
+        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests")
+    }
 
-fn test_directory() -> PathBuf {
-    PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests")
-}
+    #[test]
+    fn load_flow() -> Result<()> {
+        let base_dir = test_directory();
 
-#[test]
-fn reload_flow() -> Result<()> {
-    let base_dir = test_directory();
+        let flow_block = read_flow_block(&base_dir.join("flow.oo.yaml"))?;
+        assert_eq!(flow_block.nodes[0].concurrency(), 3);
 
-    let flow_path = find_flow(base_dir.to_str().unwrap())?;
-
-    let flow_block = read_flow_block(&flow_path)?;
-
-    assert_eq!(flow_block.nodes[0].concurrency(), 3);
-
-    Ok(())
+        Ok(())
+    }
 }
