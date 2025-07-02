7524e364331010f253470b04ae843748a75280d1
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-02 12:06:03 +0800

    feat(test): add additional check

--- manifest_meta/tests/flow_test.rs
+++ manifest_meta/tests/flow_test.rs
@@ -8,11 +8,13 @@ mod tests {
     use utils::error::Result;
 
     #[test]
-    fn test_flow_block() -> Result<()> {
+    fn test_flow_block() {
         let base_dir = test_directory();
         let mut finder = BlockPathFinder::new(base_dir, None);
         let mut block_reader = BlockResolver::new();
-        let flow_block = block_reader.resolve_flow_block("flow-1", &mut finder)?;
+        let flow_block = block_reader
+            .resolve_flow_block("flow-1", &mut finder)
+            .unwrap();
 
         assert!(flow_block.path.ends_with("flow-1/subflow.oo.yaml"));
 
@@ -36,16 +38,23 @@ mod tests {
         let node2 = flow_block.nodes.get(&node2_id).unwrap();
         assert!(matches!(node2, manifest_meta::Node::Task(_)));
         if let manifest_meta::Node::Task(task_node) = node2 {
-            let from_node1 = task_node.inputs.get(&handle_in1).unwrap();
+            let input_in1 = task_node.inputs.get(&handle_in1).unwrap();
+
+            let definition = input_in1.def.clone();
+            assert_eq!(definition.handle, handle_in1);
+            assert_eq!(definition.nullable, Some(true));
+            assert!(definition
+                .json_schema
+                .is_some_and(|schema| { schema.get("type").map_or(false, |t| t == "string") }));
 
             assert!(matches!(
-                from_node1.from.as_ref().unwrap().first().unwrap(),
+                input_in1.from.as_ref().unwrap().first().unwrap(),
                 manifest_meta::HandleSource::NodeOutput { .. }
             ));
             if let manifest_meta::HandleSource::NodeOutput {
                 node_id,
                 output_handle,
-            } = from_node1.from.as_ref().unwrap().first().unwrap()
+            } = input_in1.from.as_ref().unwrap().first().unwrap()
             {
                 assert_eq!(node_id, &node1_id);
                 assert_eq!(output_handle, &handle_out2);
@@ -74,7 +83,37 @@ mod tests {
             }
         }
 
-        Ok(())
+        let node3 = flow_block
+            .nodes
+            .get(&NodeId::new("node3".to_owned()))
+            .unwrap();
+        assert!(matches!(node3, manifest_meta::Node::Task(_)));
+        if let manifest_meta::Node::Task(task_node) = node3 {
+            let node_additional_in = task_node
+                .inputs
+                .get(&HandleName::new("additional_in".to_owned()));
+            assert!(node_additional_in.is_some());
+            let additional_inputs = node_additional_in.unwrap();
+            assert_eq!(additional_inputs.def.is_additional, true);
+
+            let block_additional_in = task_node
+                .task
+                .inputs_def
+                .as_ref()
+                .unwrap()
+                .get(&HandleName::new("additional_in".to_owned()));
+            assert!(block_additional_in.is_some());
+            assert_eq!(block_additional_in.unwrap().is_additional, true);
+
+            let block_additional_out = task_node
+                .task
+                .outputs_def
+                .as_ref()
+                .unwrap()
+                .get(&HandleName::new("additional_out".to_owned()));
+            assert!(block_additional_out.is_some());
+            assert_eq!(block_additional_out.unwrap().is_additional, true);
+        }
     }
 
     #[test]
