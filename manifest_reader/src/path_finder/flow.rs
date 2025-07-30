cffa466e868992a7bfed67f814835aca65ed9010
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-30 15:38:36 +0800

    refactor: use & instead of as_ref

--- manifest_reader/src/path_finder/flow.rs
+++ manifest_reader/src/path_finder/flow.rs
@@ -5,7 +5,7 @@ use utils::error::Result;
 use super::manifest_file::find_oo_yaml;
 
 pub fn find_flow<P: AsRef<Path>>(flow_path: P) -> Result<PathBuf> {
-    match find_oo_yaml(flow_path.as_ref(), "flow") {
+    match find_oo_yaml(&flow_path, "flow") {
         Some(path) => Ok(path.clean()),
         None => Err(utils::error::Error::new(&format!(
             "Flow {:?} not found",
