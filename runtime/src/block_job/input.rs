551b9303a97dc56547a03cd2e1284f4dc78a7ce6
leavesster <11785335+leavesster@users.noreply.github.com>
2025-08-08 11:49:10 +0800

    refactor: move out invalid_inputs

---
+++ runtime/src/block_job/input.rs
@@ -0,0 +1,96 @@
+use std::{collections::HashMap, sync::Arc};
+
+use jsonschema::validate;
+
+use manifest_meta::{HandleName, InputHandles};
+use utils::output::OutputValue;
+
+pub fn validate_inputs(
+    inputs_def: &Option<InputHandles>,
+    input_values: &HashMap<HandleName, Arc<OutputValue>>,
+) -> HashMap<HandleName, String> {
+    let mut error_handle = HashMap::new();
+
+    for (handle, wrap_value) in input_values {
+        inputs_def.as_ref().and_then(|inputs_def| inputs_def.get(handle)).map_or_else(
+            || {
+            },
+            |def| {
+                if let Some(ref json_schema) = def.json_schema {
+                    match validate(json_schema, &wrap_value.value) {
+                        Ok(()) => {}
+                        Err(err) => {
+                            error_handle.insert(
+                                handle.clone(),
+                                format!(
+                                    "input handle ({}) value ({}) is not valid. validation error: {}",
+                                    handle, wrap_value.value, err
+                                ),
+                            );
+                        }
+                    }
+                }
+            },
+        );
+    }
+
+    error_handle
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_valid_inputs() {
+        let mut inputs_def = InputHandles::default();
+        inputs_def.insert(
+            "input1".to_string().into(),
+            manifest_meta::InputHandle {
+                json_schema: Some(serde_json::json!({
+                    "type": "string",
+                })),
+                ..manifest_meta::InputHandle::new("input1".to_string().into())
+            },
+        );
+
+        let mut input_values = HashMap::new();
+        input_values.insert(
+            "input1".to_string().into(),
+            Arc::new(OutputValue {
+                value: serde_json::json!("valid string"),
+                is_json_serializable: true,
+            }),
+        );
+
+        let errors = validate_inputs(&Some(inputs_def), &input_values);
+        assert!(errors.is_empty());
+    }
+
+    #[test]
+    fn test_invalid_inputs() {
+        let mut inputs_def = InputHandles::default();
+        inputs_def.insert(
+            "input1".to_string().into(),
+            manifest_meta::InputHandle {
+                json_schema: Some(serde_json::json!({
+                    "type": "number",
+                })),
+                ..manifest_meta::InputHandle::new("input1".to_string().into())
+            },
+        );
+
+        let mut input_values = HashMap::new();
+        input_values.insert(
+            "input1".to_string().into(),
+            Arc::new(OutputValue {
+                value: serde_json::json!("valid string"),
+                is_json_serializable: true,
+            }),
+        );
+
+        let errors = validate_inputs(&Some(inputs_def), &input_values);
+        assert!(!errors.is_empty());
+        assert!(errors.contains_key(&HandleName::from("input1")));
+    }
+}
--- runtime/src/block_job/mod.rs
+++ runtime/src/block_job/mod.rs
@@ -1,7 +1,9 @@
 mod block;
+mod input;
 mod listener;
 mod service_job;
 mod task_job;
 
 pub use block::{find_upstream, run_block, BlockJobHandle, FindUpstreamArgs, RunBlockArgs};
+pub use input::validate_inputs;
 pub use task_job::{run_task_block, RunTaskBlockArgs};
