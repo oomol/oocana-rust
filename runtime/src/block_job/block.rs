678a101128b8f6709b3b8e6bcb16bd968763ee5c
leavesster <11785335+leavesster@users.noreply.github.com>
2025-08-15 11:19:40 +0800

    refactor: remove unused struct

--- runtime/src/block_job/block.rs
+++ runtime/src/block_job/block.rs
@@ -1,20 +1,9 @@
-use std::{
-    collections::{HashMap, HashSet},
-    sync::Arc,
-};
+use std::{collections::HashSet, sync::Arc};
 
-use job::{BlockInputs, BlockJobStacks, JobId, RuntimeScope};
-use mainframe::reporter::ReporterMessage;
-use manifest_meta::{
-    Block, InputDefPatchMap, NodeId, ServiceBlock, Slot, SlotBlock, SubflowBlock, TaskBlock,
-};
+use job::JobId;
+use manifest_meta::{NodeId, SubflowBlock};
 
-use super::{service_job, task_job};
-use crate::{
-    block_status::BlockStatusTx,
-    flow_job::{self, NodeInputValues},
-    shared::Shared,
-};
+use crate::flow_job::{self};
 
 pub struct BlockJobHandle {
     // TODO: Remove this field
@@ -38,24 +27,6 @@ impl BlockJobHandle {
             _job: Box::new(job),
         }
     }
-}
-
-pub struct RunBlockArgs {
-    pub block: Block,
-    pub shared: Arc<Shared>,
-    pub parent_flow: Option<Arc<SubflowBlock>>,
-    pub stacks: BlockJobStacks,
-    pub job_id: JobId,
-    pub inputs: Option<BlockInputs>,
-    pub block_status: BlockStatusTx,
-    pub nodes: Option<HashSet<NodeId>>,
-    pub node_value_store: Option<NodeInputValues>,
-    pub parent_scope: RuntimeScope,
-    pub scope: RuntimeScope,
-    pub timeout: Option<u64>,
-    pub inputs_def_patch: Option<InputDefPatchMap>,
-    pub slot_blocks: Option<HashMap<NodeId, Slot>>,
-    pub path_finder: manifest_reader::path_finder::BlockPathFinder,
 }
 
 pub struct FindUpstreamArgs {
