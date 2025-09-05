fd116fecbfe2b4ba9b4edebcb86b1e867f72d862
leavesster <11785335+leavesster@users.noreply.github.com>
2025-09-05 17:50:49 +0800

    docs: add docs

--- manifest_reader/src/manifest/node/condition.rs
+++ manifest_reader/src/manifest/node/condition.rs
@@ -8,6 +8,7 @@ use crate::{
 use super::common::{default_concurrency, default_progress_weight, NodeId};
 
 extend_node_common_field!(ConditionNode {
+    // first input handle will be condition output value
     inputs_def: Option<Vec<InputHandle>>,
     conditions: ConditionBlock,
 });
