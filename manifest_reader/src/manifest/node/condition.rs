6d1359c8723b849e507868faf8e8145ea124dd32
leavesster <11785335+leavesster@users.noreply.github.com>
2025-09-06 07:42:29 +0000

    docs

--- manifest_reader/src/manifest/node/condition.rs
+++ manifest_reader/src/manifest/node/condition.rs
@@ -9,6 +9,7 @@ use super::common::{default_concurrency, default_progress_weight, NodeId};
 
 extend_node_common_field!(ConditionNode {
     // first input handle will be condition output value
+    // maybe better keep same as other node type, and add a field to indicate which input is the condition output handle
     inputs_def: Option<Vec<InputHandle>>,
     conditions: ConditionBlock,
 });
