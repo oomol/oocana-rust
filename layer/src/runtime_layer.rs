80d2d0948ddac73e199f70e19e6a60992246fba3
leavesster <11785335+leavesster@users.noreply.github.com>
2026-01-15 10:58:34 +0800

    fix: remove unsed function

--- layer/src/runtime_layer.rs
+++ layer/src/runtime_layer.rs
@@ -7,8 +7,6 @@ use crate::ovmlayer::{self, BindPath};
 use crate::package_layer::{PackageLayer, CACHE_DIR};
 use crate::package_store::get_or_create_package_layer;
 use crate::registry_layer_store::get_or_create_registry_layer;
-use manifest_reader::path_finder::find_package_file;
-use manifest_reader::reader::read_package;
 use std::collections::HashMap;
 use std::env::temp_dir;
 use std::fmt::Debug;
@@ -48,9 +46,7 @@ pub fn create_runtime_layer(
 ) -> Result<RuntimeLayer> {
     let layer = match (package_name, version) {
         (Some(pkg_name), Some(ver)) => {
-            match get_or_create_registry_layer(
-                pkg_name, ver, package, bind_paths, envs, env_file,
-            ) {
+            match get_or_create_registry_layer(pkg_name, ver, package, bind_paths, envs, env_file) {
                 Ok(layer) => {
                     info!("runtime layer from registry store: {}@{}", pkg_name, ver);
                     Ok(layer)
