93abb7c49ad220468a0c9e7efed47fc9709172df
leavesster <11785335+leavesster@users.noreply.github.com>
2026-01-24 17:29:34 +0800

    chore: rebuild .devcontainer for better usage

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,1 +1,20 @@
-ovmlayer setup dev --base-rootfs='/workspaces/layer/base-rootfs.tar' --layer-disk=/root/tmp_overlay_desk
+set -xe
+
+# zsh history
+ln -sf /root/commandhistory/.zsh_history /root/.zsh_history
+
+arch=$(dpkg --print-architecture)
+
+# download ovmlayer
+# this repo is private, make sure you have gh cli installed and authenticated
+gh release download --repo oomol/ovmlayer-next --pattern "*${arch}*" --clobber -O /tmp/ovmlayer.tar.zst && \
+zstd -d /tmp/ovmlayer.tar.zst -o /tmp/ovmlayer.tar && \
+tar -xf /tmp/ovmlayer.tar -C /tmp
+mv /tmp/ovmlayer/ovmlayer /usr/local/bin/ovmlayer
+chmod +x /usr/local/bin/ovmlayer
+
+# download ovmlayer rootfs
+gh release download base-rootfs@0.4.0 --repo oomol/ovmlayer-rootfs --pattern "*${arch}*" --clobber -O /tmp/ubuntu-rootfs.tar
+
+# setup ovmlayer
+sudo ovmlayer setup -d /workspaces/ovmlayer-layers,rw --rootfs-tar /tmp/ubuntu-rootfs.tar --rootfs-path /workspaces/rootfs
